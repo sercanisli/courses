8c82c7aafa0d6f7149ac59f24b1dbb10e6fa61f8
Sercan ISLI <sercanislii@gmail.com>
2023-12-23 02:35:01 +0300

    The values extracted from the database were reflected in the Course component. For the delete process, the id value was moved from Course, Courses, App components respectively and realized in App.js.

--- src/App.js
+++ src/App.js
@@ -9,6 +9,11 @@ function App() {
   const [courses, setCourses] = useState([])
   const [loading, setLoading] = useState(true)
 
+  const deleteCourse = (id) =>{
+    const afterDeletedCourses = courses.filter((course) => course.id !== id);
+    setCourses(afterDeletedCourses);
+  }
+
   const fetchCourses = async () => {
     setLoading(true);
     try {
@@ -29,7 +34,7 @@ function App() {
       (
         <Loading />
       ) : (
-        <Courses courses={courses}/> 
+        <Courses courses={courses} removeCourse={deleteCourse}/> 
       )
     }
    
--- src/Courses.js
+++ src/Courses.js
@@ -1,6 +1,20 @@
-function Courses({courses}) {
-    
-    return <div></div>;
+import Course from "./Course";
+
+function Courses({courses, removeCourse}) { 
+    return <div>
+        <div>
+            <h2>Kurslarım</h2>
+        </div>
+        <div className="">
+            {
+                courses.map((course) => {
+                    return (
+                        <Course key={course.id} {...course} removeOneCourse={removeCourse}/>
+                    )
+                })
+            }
+        </div>
+    </div>;
 }
 
 export default Courses;
